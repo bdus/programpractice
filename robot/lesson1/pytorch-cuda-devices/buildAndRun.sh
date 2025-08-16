df955a634e7ec7a9f601b3df4ab3ae4ecff1f82a
bdus <rovingthrough@163.com>
2025-08-16 22:20:06 +0800

    l1

--- robot/lesson1/pytorch-cuda-devices/buildAndRun.sh
+++ robot/lesson1/pytorch-cuda-devices/buildAndRun.sh
@@ -5,3 +5,9 @@ docker build -t pytorch-cuda-devices .
 
 # 启动容器，挂载GPU
 docker run --rm --gpus all pytorch-cuda-devices
+
+# 本地运行
+sudo docker run --rm --gpus all -it pytorch/pytorch:2.8.0-cuda12.9-cudnn9-runtime /bin/bash
+
+
+docker commit <container_id> myapp_backup:v1
